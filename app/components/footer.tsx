c45547b6b020bbe576828e8eb019d25e48cc3ac2
Giacomo Panzacchi <61358600+GJace@users.noreply.github.com>
2025-07-22 10:56:52 +0200

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -46,10 +46,21 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://github.com/GJace/portfolio-next-vercel"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">view source</p>
+          </a>
+        </li>
+         <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">the template I used</p>
           </a>
         </li>
       </ul>
